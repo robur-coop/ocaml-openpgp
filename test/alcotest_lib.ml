6b34c692d0881b00aee1cc6b1a96445a2725fb23
Your Name <you@example.com>
2017-09-25 22:47:32 +0200

    tests: add integrity check that we can gen->sign->convert->verify

--- test/alcotest_lib.ml
+++ test/alcotest_lib.ml
@@ -40,6 +40,35 @@ let test_gnupg_key_002 () =
      |> R.reword_error (function `Msg s -> failwith s)
   ) ; ()
 
+let test_integrity_dsa () =
+  let uid = "My name goes here" in
+  let message_cs = Cstruct.of_string "my message" in
+
+  let g = Nocrypto_entropy_unix.initialize() ; !Nocrypto.Rng.generator in
+ (
+  Public_key_packet.generate_new ~current_time ~g DSA >>= fun root_sk ->
+  Public_key_packet.generate_new ~current_time ~g DSA >>= fun subkey_sk ->
+  Openpgp.Signature.sign_detached_cs ~current_time ~g root_sk Types.SHA384
+    message_cs
+  >>= (fun sig_t -> Openpgp.serialize_packet Types.V4
+                      (Openpgp.Signature_type sig_t)
+      )>>| Openpgp.encode_ascii_armor Types.Ascii_signature >>= fun sig_cs ->
+  Openpgp.new_transferable_secret_key ~g ~current_time Types.V4 root_sk
+                                      [uid] [subkey_sk] >>= fun sk ->
+
+  Openpgp.serialize_transferable_secret_key Types.V4 sk
+  >>| Openpgp.encode_ascii_armor Types.Ascii_private_key_block
+  >>= Openpgp.decode_secret_key_block ~current_time >>| fst
+  >>| Openpgp.Signature.transferable_public_key_of_transferable_secret_key
+  >>= fun root_pk ->
+  Openpgp.decode_detached_signature sig_cs
+  >>= fun signature -> Openpgp.Signature.verify_detached_cs ~current_time
+                            root_pk signature message_cs
+ ) |> function | Ok `Good_signature -> ()
+               | Error (`Msg s) -> failwith s
+               | Error `Incomplete_packet -> failwith "incomplete packet"
+
+
 let tests =
   [
     "packet length encoding",
@@ -51,7 +80,9 @@ let tests =
     ; "GnuPG RSA-SC + RSA-E (001)", `Slow, test_gnupg_key_001
     ; "GnuPG RSA-SC + RSA-S (002)", `Slow, test_gnupg_key_002
     ]
-  ;
+  ; "Integrity checks",
+    [ "DSA: generate; sign; convert; verify", `Slow, test_integrity_dsa ;
+    ]
   ]
 
 let () =
