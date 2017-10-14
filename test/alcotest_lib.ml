496c4da6b35b05ff5fcd8edddee3f7e639e7a9bc
Your Name <you@example.com>
2017-10-14 15:35:56 +0200

    test cases for deterministic keygen

--- test/alcotest_lib.ml
+++ test/alcotest_lib.ml
@@ -1,6 +1,8 @@
 open Types
 open Rresult
 
+let a_cs = Alcotest.testable Cstruct.hexdump_pp Cstruct.equal
+
 let test_packet_length_selfcheck () =
   Alcotest.(check int32) "same int" 1234l
     (serialize_packet_length_uint32 1234l
@@ -69,7 +71,7 @@ let test_gnupg_key_001 () =
   |> R.reword_error (function `Msg s -> failwith s) in ()
 
 let test_gnupg_key_002 () =
-  ignore (
+  let _ =
     (cs_of_file "test/keys/gnupg.test.002.pk.asc"
      >>= Openpgp.decode_public_key_block ~current_time) >>| fst >>= fun pk ->
      key_has_uids 1 pk >>= fun () -> key_has_subkeys 1 pk >>= fun () ->
@@ -77,16 +79,18 @@ let test_gnupg_key_002 () =
      >>= Openpgp.decode_detached_signature  >>= fun detach_sig ->
      cs_of_file "test/keys/message.001.txt" >>= fun msg ->
      Openpgp.Signature.verify_detached_cs ~current_time pk detach_sig msg
-     |> R.reword_error (function `Msg s -> failwith s)
-  ) ; ()
+     |> R.reword_error (function `Msg s -> failwith s) in ()
 
 (* TODO test that checks that we do not validate signatures with RSA_encrypt *)
 
-let test_integrity_with_algo algo =
+let test_integrity_with_algo algo target_hashes : unit =
   let uid = "My name goes here" in
   let message_cs = Cstruct.of_string "my message" in
-  let g = Nocrypto_entropy_unix.initialize() ; !Nocrypto.Rng.generator in
-  (
+  let current_time = Ptime.Span.of_int_s 1234567890 |> Ptime.of_span
+                     |> function Some time -> time | None -> failwith "x" in
+  let g = let seed = Cs.of_string "a deterministic seed 9e5cbce8"in
+    Nocrypto.Rng.create ~seed (module Nocrypto.Rng.Generators.Fortuna) in
+  ( Nocrypto_entropy_unix.initialize () ; (*TODO document somewhere that this is required *)
   Public_key_packet.generate_new ~current_time ~g algo >>= fun root_sk ->
   Public_key_packet.generate_new ~current_time ~g algo >>= fun subkey_sk ->
   Openpgp.Signature.sign_detached_cs ~current_time root_sk Types.SHA384
@@ -99,19 +103,34 @@ let test_integrity_with_algo algo =
 
   Openpgp.serialize_transferable_secret_key Types.V4 sk
   >>| Openpgp.encode_ascii_armor Types.Ascii_private_key_block
-  >>= Openpgp.decode_secret_key_block ~current_time >>| fst
+  >>= fun sk_asc_cs ->
+  Openpgp.decode_secret_key_block ~current_time sk_asc_cs >>| fst
   >>| Openpgp.Signature.transferable_public_key_of_transferable_secret_key
   >>= fun root_pk ->
   key_has_uids 1 root_pk >>= fun () -> key_has_subkeys 1 root_pk >>= fun () ->
   Openpgp.decode_detached_signature sig_cs
   >>= fun signature -> Openpgp.Signature.verify_detached_cs ~current_time
-                            root_pk signature message_cs
- ) |> function | Ok `Good_signature -> ()
-               | Error (`Msg s) -> failwith s
-               | Error `Incomplete_packet -> failwith "incomplete packet"
+    root_pk signature message_cs
+  >>| fun `Good_signature ->
+   let hashes = List.map Nocrypto.Hash.MD5.digest [sk_asc_cs; sig_cs] in
+   Logs.app (fun m -> m "%a" (Fmt.list Cstruct.hexdump_pp) hashes);
+   Alcotest.(check @@ list a_cs) "deterministic generation of secret key & sig"
+     hashes
+     (List.map (fun h -> Cs.of_hex h |> R.get_ok)target_hashes);
+   `Good_signature
+  ) |> function | Ok `Good_signature -> ()
+                | Error (`Msg s) -> failwith s
+                | Error `Incomplete_packet -> failwith "incomplete packet"
 
-let test_integrity_dsa () = test_integrity_with_algo DSA
-let test_integrity_rsa () = test_integrity_with_algo RSA_encrypt_or_sign
+
+let test_integrity_dsa () : unit =
+  ["aa15898f91a57a0428d61aca60fcf244";
+   "2b8d620fa5c755b1d34c570e36588d15"]
+  |> test_integrity_with_algo DSA
+let test_integrity_rsa () : unit =
+  ["085fd7cd79b26984d46c937e6b2f12ed";
+   "a86680d3768b7e4f923f87305f7b995e"]
+  |> test_integrity_with_algo RSA_encrypt_or_sign
 
 let tests =
   [
